df62a643c24244261431dce2df0aa16c30831414
jugbeep <patrickbotkins@gmail.com>
2018-01-11 10:50:37 -0700

    face schema set up

--- server.js
+++ server.js
@@ -2,10 +2,36 @@
 const express = require('express');
 const app = express();
 
+/// set up image schema ///
+let mongoose = require('mongoose');
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/Face-App' );
+
+let Schema = mongoose.Schema;
+
+let faceSchema = ({
+	name: String,
+	photoUrl: String,
+	likes: [ String ],
+	dislikes: [ String ],
+	hometown: String,
+	specialRequests: String,
+	significantOther: [ String ],
+	kids: String,
+	birthday: Number,
+	favoriteTeam: String,
+	miscNotes: String
+});
+
+let Recongnize = mongoose.model('Recongnize', faceSchema);
+
+
+/// home path get ///
 app.get('/home', function (req, res){
 	res.send('hello from root get');
 });
 
+
+/// setting up port to listen ///
 app.listen(process.env.PORT || 3000, function() {
  		console.log('listening on port 3000');
 })
