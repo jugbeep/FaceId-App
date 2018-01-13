6fce669a0e19f9dda16e35efa8e0662b19fbfffd
jugbeep <patrickbotkins@gmail.com>
2018-01-13 13:30:34 -0700

    tyring heroku again

---
+++ public/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	console.log('doc is ready');
+
+
+$.get('https://super-crud.herokuapp.com/books')
+    .done(function(data){
+      console.log('hit ajax get route');
+    });
+
+// /// ajax ///
+
+// let headers = {
+// 	url: 'https://api.kairos.com/v2/media/',
+// 	headers: {
+// 		'app_id': id,
+// 		'app_key': key
+// 	}
+// };
+
+// $('.btn btn-primary').submit(function(){
+// 	event.preventDefault();
+	
+// 	request(headers, function(err, res, body) {
+// 		let resData = JSON.parse(body);
+// 		console.log(resData);
+// 		res.send(body);
+// 	});
+// });
+});
