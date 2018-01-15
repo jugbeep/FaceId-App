21d96f5d15cd2fadee9a03c6a73ad140d27fdbf5
jugbeep <patrickbotkins@gmail.com>
2018-01-15 09:20:11 -0700

    removing sensitive stuff

--- apiFaceCalls.js
+++ apiFaceCalls.js
@@ -7,14 +7,5 @@ const request = require('request');
 
 get();
 
-POST /v2/media?source=http://www.dreams.metroeve.com/wp-content/uploads/2017/05/dreams.metroeve_face-dreams-meaning.jpg HTTP/1.1
-Host: api.kairos.com
-app_id: 1fe49d29
-app_key: e56f8d42741b2d22d56e38d57214b073
-Cache-Control: no-cache
-Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW
-
-
-
 module.exports = get;
 
