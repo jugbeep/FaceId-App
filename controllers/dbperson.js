e67817561ee0543b5175a3370d1ae198982d1882
jugbeep <patrickbotkins@gmail.com>
2018-01-16 15:22:53 -0700

    dynamic name search

--- controllers/dbperson.js
+++ controllers/dbperson.js
@@ -2,11 +2,13 @@ const db = require('mongodb').MongoClient;
 const url = process.env.MONGODB_URI || 'mongodb://localhost/Face-App'
 
 function find(req, res) {
+	let name = req.query.name;
 
 	db.connect(url, function(err, db) {
+
 		let collection = db.collection('recognizes');
 
-		collection.findOne({'name': 'Patrick'}, function(err, doc) {
+		collection.findOne({'name': name }, function(err, doc) {
 			if (err) console.log(err)
 			
 			res.render('personDoc', {doc:doc});
